17240741dba73c538f03583495b9c64fac3e2088
chillout2san <chillout2san@gmail.com>
2023-03-10 22:18:43 +0900

    implement mininum tokens

---
+++ token.go
@@ -0,0 +1,17 @@
+package parser
+
+type TokenType int
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	LEFT_BRACE TokenType = iota
+	RIGHT_BRACE
+	DOUBLE_QUOTE
+	COLON
+	KEY
+	VALUE
+)
